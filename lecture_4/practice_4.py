9d0cce9e4beab4a1a5038a14e621c20baf7d5fac
mariiabrynskaia <m.brynskaia.sas@gmail.com>
2023-09-08 23:13:30 +0300

    final

--- lecture_4/practice_4.py
+++ lecture_4/practice_4.py
@@ -1,6 +1,7 @@
 from import_this import generate_race_data
 
-def to_sort_spisok (race_data: dict) -> dict:
+
+def to_sort_spisok(race_data: dict):
     new_sorted_dict: dict = {}
     for i in race_data.values():
         new_sorted_place: int = i['FinishedPlace']
@@ -8,21 +9,20 @@ def to_sort_spisok (race_data: dict) -> dict:
     return new_sorted_dict
 
 
-def first_place (new_sorted_dict: dict) -> str:
-    first_place_name: str = f'Выиграл - {new_sorted_dict[1]["RacerName"]}!!!! Поздравляем!!!'
+def first_place(new_sorted_dict: dict):
+    first_place_name: str = f'Выиграл(а) - {new_sorted_dict[1]["RacerName"]}!!!! Поздравляем!!!'
     print(first_place_name + '\n' + '-'*len(first_place_name))
 
 
-def all_victory_places (new_sorted_dict: dict) -> str:
+def all_victory_places(new_sorted_dict: dict) -> str:
     print('Первые 3 места:\n')
-    for k in range(1,4):
+    for k in range(1, 4):
         print(f'Гонщик на {k} месте\n' +
               '\t' + f'Имя: {new_sorted_dict[k]["RacerName"]}\n' +
               '\t' + f'Команда: {new_sorted_dict[k]["RacerTeam"]}\n' +
               '\t' + f'Время: {new_sorted_dict[k]["FinishedTimeSeconds"] // 3600}:' +
-              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 3600 // 60}:'+
-              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 60}'
-        )
+              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 3600 // 60}:' +
+              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 60}\n')
 
 
 if __name__ == '__main__':
