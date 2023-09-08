4dd93e7ac062e972b44e5793ce482757fc5c1ad3
mariiabrynskaia <m.brynskaia.sas@gmail.com>
2023-09-08 22:37:38 +0300

    uraaaa

--- lecture_4/practice_4.py
+++ lecture_4/practice_4.py
@@ -1,39 +1,31 @@
-from import_this import*
-#from import_this import RACE_DATA
+from import_this import generate_race_data
 
-# def count_winners (x: int | str | float | dict [int | str | float, int | str | float]) -> int | str | float | dict [int | str | float, int | str | float]:
-
-#     for key in RACE_DATA.items():   
-#         for key in RACE_DATA.get(key):
-#             if 'FinishedPlace' <=3:
-#                  return RACE_DATA.get(key)
-#     return 'There is nothing'
-newstring: str = []
-#print(RACE_DATA.get(1))
-for key, value in RACE_DATA.items():
-    for key in range(1, 6):
-        newstring.append(value)
-        print(newstring(1))
-        #if new_list[2] <=3:
-        #    print(f'Призер {new_list(0)}, занял(а) {2} место')
-        
-    
+def to_sort_spisok (race_data: dict) -> dict:
+    new_sorted_dict: dict = {}
+    for i in race_data.values():
+        new_sorted_place: int = i['FinishedPlace']
+        new_sorted_dict[new_sorted_place] = i
+    return new_sorted_dict
 
 
+def first_place (new_sorted_dict: dict) -> str:
+    first_place_name: str = f'Выиграл - {new_sorted_dict[1]["RacerName"]}!!!! Поздравляем!!!'
+    print(first_place_name + '\n' + '-'*len(first_place_name))
 
 
+def all_victory_places (new_sorted_dict: dict) -> str:
+    print('Первые 3 места:\n')
+    for k in range(1,4):
+        print(f'Гонщик на {k} месте\n' +
+              '\t' + f'Имя: {new_sorted_dict[k]["RacerName"]}\n' +
+              '\t' + f'Команда: {new_sorted_dict[k]["RacerTeam"]}\n' +
+              '\t' + f'Время: {new_sorted_dict[k]["FinishedTimeSeconds"] // 3600}:' +
+              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 3600 // 60}:'+
+              f'{new_sorted_dict[k]["FinishedTimeSeconds"] % 60}'
+        )
 
-# def count (x: int | str | float | dict [int | str | float, int | str | float]) -> int | str | float:
 
-#     for key in RACE_DATA.items():
-#         if key == 1:
-#             for key, value in RACE_DATA.items():
-#                 print(f'Победитель: {key} {value}')
-#         elif 'FinishedPlace' == 2:
-#             for key, value in RACE_DATA.items():
-#                 print(f'Второе место: {key} {value}')
-#         elif 'FinishedPlace' == 3:
-#             for key, value in RACE_DATA.items():
-#                 print(f'Третье место: {key} {value}')
-# answer = count(RACE_DATA.keys)
-# print (answer)
+if __name__ == '__main__':
+    final_dict: dict = to_sort_spisok(generate_race_data(10))
+    first_place(final_dict)
+    all_victory_places(final_dict)
